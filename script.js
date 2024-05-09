6f353a441654bb01d9f68fe026b93c4b10a747f6
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-05-09 00:51:27 -0400

    Create the grid

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const container = document.querySelector("container");
+const container = document.querySelector("#container");
+    for(let i = 1; i < 257; i++){
+        const square = document.createElement("div");
+        square.className = "cube";
+        square.textContent = i ;
+
+        container.appendChild(square);
+    }
