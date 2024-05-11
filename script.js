ff3f8258a7d18d539aeecbd7fe45a59187094d5b
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-05-10 22:56:51 -0400

    Make the squares fit the container
    a change in the distribution of space in the css and erase the line of js that generate the number of the square

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const container = document.querySelector("#container");
-const initialCanvas= firstCanvas();
-let userWidth=0;
-let userHeight=0;
+let userInput=0;
 
-function firstCanvas(){
     for(let i = 1; i < 257; i++){
         const square = document.createElement("div");
         square.className = "cube";
-        square.textContent = i ;
         square.addEventListener("mouseenter", function( event ) {   
             event.target.style.background = "purple";});
 
         container.appendChild(square);
     }
+
+
+function userCanvas(){
+    const buttonAsk = document.querySelector("#newCanvas")
+    buttonAsk.addEventListener("click", () => {
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);}
+        userInput = prompt("Please enter the new canvas size")
+            if (userInput < 100) {
+                for(let i = 1; i < userInput*userInput+1; i++){
+                    const square = document.createElement("div");
+                    square.className = "cubejs";
+                    square.style.setProperty("--widthInput", +userInput);
+                    square.style.setProperty("--heightInput", +userInput);
+                    square.addEventListener("mouseenter", function( event ) {   
+                        event.target.style.background = "purple";});
+            
+                    container.appendChild(square);
+                }
+
+            } else {
+                alert("Please try again")
+            }
+    })
+
 }
+userCanvas();
